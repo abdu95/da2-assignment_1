2a7c8b2ae8670805dc0a449fcb63a1001a3b69a3
abdumalik2014 <abdumalik2014@gmail.com>
2020-11-28 22:00:10 +0100

    Update README.md

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ statistical reasoning:
 4. my dependent (y) variable is *Number of registered death* and explanatory (x) variable is *Number of registered case*.
 5. which observation(s) to drop >> extreme values. 
 6. what to do to change 3.386e+04 to normal number?
+7. Canada and Chile NA: Princess in Canada, drop Princess first?
+8. how to determine the breaks()? 
+9. Choose a proper scaling (I think with this, I can get rid of e+04)
+10. Show the model results >> model results are alpha and beta?
+11. Read Evaluation
--- codes/analysis_covid_data.R
+++ codes/analysis_covid_data.R
@@ -30,10 +30,14 @@ library(texreg)
 #install.packages(ggthemes)
 library(ggthemes)
 
-# Call the data from file
+# the file path 
 my_file <- "../data/clean/covid_pop_09_11_2020_clean.csv"
+
+# make dataframe from a file
 df <- read_csv(my_file)
 
+# drop extreme values
+df <- subset(df, df$population < 300000000)
 
 ####
 # 
@@ -50,16 +54,51 @@ df %>%
 
 summary( df )
 
-what <- select(df, df$population < 300000000)
-what <- subset(df, df$population < 300000000)
+# df %>% filter(!complete.cases(df))
+
 ######
 # Check basic scatter-plot:
-#     No_of_death = alpha + beta * No_of_cases
+#     No_of_death = alpha + beta * No_of_confirmed
 #
 # Where to use log-transformation? - level-level vs level-log vs log-level vs log-log
 #
-# 1) death - No_of_cases: level-level model without scaling
+# No_of_death - No_of_confirmed: level-level model without scaling
 ggplot( df , aes(x = confirmed, y = death)) +
   geom_point() +
   geom_smooth(method="loess")+
   labs(x = "Number of registered case (31 October 2020)",y = "Number of registered death") 
+
+
+# changing the scale for No_of_death for checking log-transformation: level y - log x 
+# looks good
+# log log and level log
+ggplot( df , aes(x = confirmed, y = death)) +
+  geom_point() +
+  geom_smooth(method="loess")+
+  labs(x = "Number of registered case (31 October 2020, ln scale)",y = "Number of registered death") +
+  scale_x_continuous( trans = log_trans(),  breaks = c(1,2,5,10,20,50,100,200,500,1000,10000) )
+
+# changing the scale for No_of_death for checking log-transformation: log y - level x 
+ggplot( df , aes(x = confirmed, y = death)) +
+  geom_point() +
+  geom_smooth(method="loess")+
+  labs(x = "Number of registered case (31 October 2020)",y = "Number of registered death (ln scale)") +
+  scale_y_continuous( trans = log_trans(),  breaks = c(1,2,5,10,20,50,100,200,500,1000,10000) )
+
+# changing the scale for No_of_death and No_of_confirmed for checking log-transformation: log y - log x
+ggplot( df , aes(x = confirmed, y = death ))  +
+  geom_point() +
+  geom_smooth(method="loess")+
+  labs(x = "Number of registered case (31 October 2020, ln scale)",y = "Number of registered death (ln scale)") +
+  scale_x_continuous( trans = log_trans(),  breaks = c(1,2,5,10,20,50,100,200,500,1000,10000) )+
+  scale_y_continuous( trans = log_trans() )
+
+
+####
+# Conclusions:
+
+# - Substantive: unit of measurement
+# - Statistical: avoiding long-right tail, more symmetric
+
+
+
